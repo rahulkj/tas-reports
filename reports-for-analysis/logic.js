2d82c7d3042aecaea63ca86e1bc3c9250ea5ff2d
Rahul Jain <rahulkj@gmail.com>
2024-01-03 19:51:07 -0600

    Adding more reports

--- reports-for-analysis/logic.js
+++ reports-for-analysis/logic.js
@@ -1,65 +1,113 @@
 function getBuildpackChartData() {
-    let buildpackRelationData = {"name": data.envName, "children": []}
+  let buildpackRelationData = { name: data.envName, children: [] };
 
-    for (const org of data.orgs) {
+  for (const org of data.orgs) {
+    let orgData = { name: org.orgName, children: [] };
 
-        let orgData = {name: org.orgName, children: []}
+    for (const space of org.spaces) {
+      let spaceData = { name: space.spaceName, children: [] };
 
-        for (const space of org.spaces) {
-            let spaceData = {"name": space.spaceName, "children": []}
+      for (const spaceApp of space.apps) {
+        let buildpackName = spaceApp.entity.detected_buildpack;
 
-            for (const spaceApp of space.apps) {
-                let buildpackName = spaceApp.entity.detected_buildpack
+        let appName = spaceApp.entity.name;
 
-                let appName = spaceApp.entity.name
+        let appData = { name: appName, value: spaceApp.entity.instances };
 
-                let appData = {name: appName, value: spaceApp.entity.instances}
+        let foundAndUpdatedBuildpack = false;
 
-                let foundAndUpdatedBuildpack = false
-
-                for (const buildpack of spaceData.children) {
-                    if (buildpack.name === buildpackName) {
-                        buildpack.children.push(appData)
-                        foundAndUpdatedBuildpack = true
-                    }
-                }
-
-                if (!foundAndUpdatedBuildpack) {
-                    let buildpackData = {"name": buildpackName, children: []}
-                    buildpackData.children.push(appData)
-                    spaceData.children.push(buildpackData)
-                }
-            }
-
-            orgData.children.push(spaceData)
+        for (const buildpack of spaceData.children) {
+          if (buildpack.name === buildpackName) {
+            buildpack.children.push(appData);
+            foundAndUpdatedBuildpack = true;
+          }
         }
 
-        buildpackRelationData.children.push(orgData)
+        if (!foundAndUpdatedBuildpack) {
+          let buildpackData = { name: buildpackName, children: [] };
+          buildpackData.children.push(appData);
+          spaceData.children.push(buildpackData);
+        }
+      }
 
+      orgData.children.push(spaceData);
     }
 
-    return buildpackRelationData
+    buildpackRelationData.children.push(orgData);
+  }
+
+  return buildpackRelationData;
+}
+
+function getOrgSpaceAppsChartData() {
+  let buildpackRelationData = { name: data.envName, children: [] };
+
+  for (const org of data.orgs) {
+    let orgData = { name: org.orgName, children: [] };
+
+    for (const space of org.spaces) {
+      let spaceData = { name: space.spaceName, children: [] };
+
+      for (const spaceApp of space.apps) {
+        let appName = spaceApp.entity.name;
+
+        let appData = { name: appName, value: spaceApp.entity.instances };
+
+        spaceData.children.push(appData);
+      }
+
+      orgData.children.push(spaceData);
+    }
+
+    buildpackRelationData.children.push(orgData);
+  }
+
+  return buildpackRelationData;
 }
 
 function getAppResourceData() {
-    let renderData = {apps: [], instances: [], memoryUsage: [], diskQuota: []}
+  let renderData = { apps: [], instances: [], memoryUsage: [], diskQuota: [] };
 
-    for (const org of data.orgs) {
+  for (const org of data.orgs) {
+    let orgData = { name: org.orgName, children: [] };
 
-        let orgData = {name: org.orgName, children: []}
+    for (const space of org.spaces) {
+      let spaceData = { name: space.spaceName, children: [] };
 
-        for (const space of org.spaces) {
-            let spaceData = {"name": space.spaceName, "children": []}
-
-            for (const spaceApp of space.apps) {
-                renderData.apps.push(spaceApp.entity.name)
-                renderData.instances.push(spaceApp.entity.instances)
-                renderData.memoryUsage.push(spaceApp.entity.memory/1024)
-                renderData.diskQuota.push(spaceApp.entity.disk_quota/1024)
-            }
-        }
-
+      for (const spaceApp of space.apps) {
+        renderData.apps.push(spaceApp.entity.name);
+        renderData.instances.push(spaceApp.entity.instances);
+        renderData.memoryUsage.push(spaceApp.entity.memory / 1024);
+        renderData.diskQuota.push(spaceApp.entity.disk_quota / 1024);
+      }
     }
+  }
 
-    return renderData
+  return renderData;
+}
+
+function getStackAppData() {
+  let stackAppData = [];
+
+  const stackAppMap = new Map();
+
+  for (const org of data.orgs) {
+    for (const space of org.spaces) {
+      for (const spaceApp of space.apps) {
+        if (stackAppMap.has(spaceApp.entity.stack)) {
+          let count = stackAppMap.get(spaceApp.entity.stack) + 1;
+          stackAppMap.set(spaceApp.entity.stack, count);
+        } else {
+          stackAppMap.set(spaceApp.entity.stack, 1);
+        }
+      }
+    }
+  }
+
+  stackAppMap.forEach(function (value, key) {
+    let stackApp = {name: key, value: value}
+    stackAppData.push(stackApp)
+  });
+
+  return stackAppData;
 }
